733352c3b024d4f029208b12733fec3f5d2a4246
ranaibrahim <rana.ibrahimhassan@yahoo.com>
2019-12-25 05:40:01 +0200

    1

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 pg = require('pg');
 
-module.exports = new Sequelize('opera', 'postgres', 'postgres', {
+module.exports = new Sequelize('opera', 'postgres', '1234567890', {
     dialect: 'postgres'
 });
 
